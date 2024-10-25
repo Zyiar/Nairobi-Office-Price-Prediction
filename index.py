29fb5798b88d3c1ae5814a0755207884f5f03ac7
Zyiar <dianahamuli@gmail.com>
2024-10-25 10:10:47 +0300

    Main code file with the regression model

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Nairobi Office Price Ex.csv')
+df = pd.read_csv('Nairobi-Office-Price-Ex.csv')
 
 office_size = df['SIZE'].values
 office_price = df['PRICE'].values
